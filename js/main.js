4a9228cf7c2dbabd10a1041fb23f521153268356
CGRepo <paulhare93@gmail.com>
2020-08-08 22:04:07 +0200

    Działa ale bez pierwszej liczby

--- js/main.js
+++ js/main.js
@@ -1,32 +1,52 @@
 let isOdd;
 let camelCaseCombiner;
+let sortNumbers;
+let countWords;
 
-isOdd = function(number){
-  if (number % 2){
+
+isOdd = function (number) {
+  if (number % 2) {
     alert("Entered number: " + number + " is Odd number!");
     number = "123";
     return;
-  }else {
+  } else {
     alert("Entered number: " + number + " is not Odd number!");
     number = "123";
     return;
   }
 }
 camelCaseCombiner = function (string) {
-  let words = [];
-  let camelCasedWords;
   let camelCasedWordsBlended = '';
- words = string.split(' ');
-  console.log(words);
- camelCasedWords = words.map(function (indexValue,indexNumber ) {
-   if (indexNumber === 0){
-     camelCasedWordsBlended += indexValue[0].toLowerCase() + indexValue.slice(1);
-     console.log(indexValue + "," + indexNumber);
-   }else{
-     camelCasedWordsBlended +=  indexValue[0].toUpperCase() + indexValue.slice(1);
-   console.log(indexValue + "," + indexNumber);
-   }
- })
+  string.split(' ').map(function (indexValue, indexNumber) {
+    if (indexNumber === 0) {
+      camelCasedWordsBlended += indexValue[0].toLowerCase() + indexValue.slice(1).toLowerCase();
+    } else {
+      camelCasedWordsBlended += indexValue[0].toUpperCase() + indexValue.slice(1).toLowerCase();
+    }
+  })
   alert(camelCasedWordsBlended);
 
+}
+sortNumbers = function (numbers) {
+  let numberArray = numbers.split(' ').map(function (a) {
+    return parseInt(a);
+  })
+// numberArray.sort(function(a, b){return a - b});
+for (let i = 0; i < numberArray.length; i++) {
+  if (numberArray[i] > numberArray[i + 1]) {
+    let temp = numberArray[i];
+    numberArray[i] = numberArray[i+1];
+    numberArray[i+1] = temp;
+  }
+
+}
+alert(numberArray);
+}
+
+countWords = function (words) {
+  let wordsArray;
+  wordsArray = words.split(' ');
+  wordsArray.sort();
+  sk
+  console.log(wordsArray);
 }
